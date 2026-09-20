b1a3969ca35b9a35f39ce7eb271d989765fafdf3
lckocherhans-crypto <lckocherhans@gmail.com>
2026-09-20 17:23:24 -0600

    Update TeamsExpansion.java

--- src/main/java/com/example/teams/TeamsExpansion.java
+++ src/main/java/com/example/teams/TeamsExpansion.java
@@ -8,22 +8,6 @@ import org.jetbrains.annotations.NotNull;
  * PlaceholderAPI expansion so other plugins (TAB, scoreboards, chat formatters) can show team info.
  *
  *   %teams_tag%     ABC            (empty if no team)
- *   %teams_prefix%  &c[ABC]&r      (coloured tag + trailing space, empty if no team)
- *   %teams_color%   &c             (team colour code, empty if no team)
- *   %teams_name%    TeamName
- *   %teams_role%    Owner / Officer / Member
- *   %teams_kills%   12
- *   %teams_members% 3
-package com.example.teams;
-
-import me.clip.placeholderapi.expansion.PlaceholderExpansion;
-import org.bukkit.OfflinePlayer;
-import org.jetbrains.annotations.NotNull;
-
-/**
- * PlaceholderAPI expansion so other plugins (TAB, scoreboards, chat formatters) can show team info.
- *
- *   %teams_tag%     ABC            (empty if no team)
  *   %teams_prefix%  &#FF5555[ABC]&r      (coloured tag + trailing space, empty if no team)
  *   %teams_color%   &#FF5555            (team colour code, empty if no team)
  *   %teams_name%    TeamName
@@ -52,8 +36,8 @@ public final class TeamsExpansion extends PlaceholderExpansion {
 
         return switch (params.toLowerCase()) {
             case "tag" -> t.tag();
-            case "color" -> t.color().hexCode();
-            case "prefix" -> t.color().hexCode() + "[" + t.tag() + "] &r";
+            case "color" -> t.primaryCode();
+            case "prefix" -> t.tabColorize("[" + t.tag() + "]") + " &r";
             case "name" -> t.name();
             case "role" -> t.role(player.getUniqueId()).display();
             case "kills" -> String.valueOf(t.kills());
