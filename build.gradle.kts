1b6393a0295ef9b87f36b7e449ad6184c6fe818b
lckocherhans-crypto <lckocherhans@gmail.com>
2026-09-19 20:20:54 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,12 @@ version = "1.0.0"
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.extendedclip.com/releases/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
 java {
