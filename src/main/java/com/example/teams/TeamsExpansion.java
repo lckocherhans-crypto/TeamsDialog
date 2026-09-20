c3f3773b3dbae5a2b0c264017ecd335aad37ca2d
lckocherhans-crypto <lckocherhans@gmail.com>
2026-09-20 17:06:12 -0600

    Update TeamsExpansion.java

--- src/main/java/com/example/teams/TeamsExpansion.java
+++ src/main/java/com/example/teams/TeamsExpansion.java
@@ -14,6 +14,22 @@ import org.jetbrains.annotations.NotNull;
  *   %teams_role%    Owner / Officer / Member
  *   %teams_kills%   12
  *   %teams_members% 3
+package com.example.teams;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * PlaceholderAPI expansion so other plugins (TAB, scoreboards, chat formatters) can show team info.
+ *
+ *   %teams_tag%     ABC            (empty if no team)
+ *   %teams_prefix%  &#FF5555[ABC]&r      (coloured tag + trailing space, empty if no team)
+ *   %teams_color%   &#FF5555            (team colour code, empty if no team)
+ *   %teams_name%    TeamName
+ *   %teams_role%    Owner / Officer / Member
+ *   %teams_kills%   12
+ *   %teams_members% 3
  */
 public final class TeamsExpansion extends PlaceholderExpansion {
 
@@ -36,32 +52,13 @@ public final class TeamsExpansion extends PlaceholderExpansion {
 
         return switch (params.toLowerCase()) {
             case "tag" -> t.tag();
-            case "color" -> "&" + code(t.color());
-            case "prefix" -> "&" + code(t.color()) + "[" + t.tag() + "] &r";
+            case "color" -> t.color().hexCode();
+            case "prefix" -> t.color().hexCode() + "[" + t.tag() + "] &r";
             case "name" -> t.name();
             case "role" -> t.role(player.getUniqueId()).display();
             case "kills" -> String.valueOf(t.kills());
             case "members" -> String.valueOf(t.members().size());
             default -> null;
-        };
-    }
-
-    private static char code(TeamColor c) {
-        return switch (c) {
-            case RED -> 'c';
-            case GOLD -> '6';
-            case YELLOW -> 'e';
-            case GREEN -> 'a';
-            case DARK_GREEN -> '2';
-            case AQUA -> 'b';
-            case DARK_AQUA -> '3';
-            case BLUE -> '9';
-            case DARK_BLUE -> '1';
-            case LIGHT_PURPLE -> 'd';
-            case DARK_PURPLE -> '5';
-            case WHITE -> 'f';
-            case GRAY -> '7';
-            case DARK_GRAY -> '8';
         };
     }
 }
